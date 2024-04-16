a12b0f181883dfc83776f8046d86c9b84d9795e5
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-16 18:51:00 +0300

    Categories (#2)
    * CI: update workflow to invoke DEV deployment
    
    * CI: update pipeline to include dispatch event
    
    * CI: invoke Dev workflow once manually triggered
    
    * added categories
    
    * Update Apps.AmazonRedshift.csproj
    
    ---------
    
    Co-authored-by: Ihor Kriatov <ihor.kriatov@blackbird.io>

--- Apps.AmazonRedshift/AmazonRedshiftApplication.cs
+++ Apps.AmazonRedshift/AmazonRedshiftApplication.cs
@@ -1,9 +1,16 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.AmazonRedshift;
 
-public class AmazonRedshiftApplication : IApplication
+public class AmazonRedshiftApplication : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.AmazonApps, ApplicationCategory.DatabaseAndSpreadsheet];
+        set { }
+    }
+    
     public string Name
     {
         get => "Amazon Redshift";
